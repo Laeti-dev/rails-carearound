89aa18b1774c7c5e225e53f2ef0a624b944557fe
Taddei Laetitia <81339703+Laeti-dev@users.noreply.github.com>
2022-06-04 14:50:52 +0000

    created cosmetic_test

---
+++ test/system/cosmetics_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CosmeticsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit cosmetics_url
+  #
+  #   assert_selector "h1", text: "Cosmetic"
+  # end
+end
